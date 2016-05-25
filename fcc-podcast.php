0deff5592370f00c8ba55c421b8775afd731be68
ryan.veitch <ryan.r.veitch@gmail.com>
2016-05-25 10:14:25 -0500

    Starter files added

---
+++ fcc-podcast.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: FCC Podcast
+ * Description: General use podcasts plugin for FCC employees with JW player content.
+ * Plugin URI:  https://github.com/openfcci/fcc-podcast/
+ * Author:      Forum Communications Company
+ * Author URI:  http://www.forumcomm.com/
+ * License:     GPL v2 or later
+ * Version:     1.16.05.25
+ */
